1ce62602cc4e759907889be9487bb469d8bddeed
cpal <alvinyan819@gmail.com>
2025-04-06 20:59:11 +1000

    Visualise KELP price data

--- Tutorial/visualise.py
+++ Tutorial/visualise.py
@@ -1,11 +1,20 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("tutorial_data.csv", sep=';')
 data = pd.DataFrame(data)
 
-kelp_data = data[data["product"] == "KELP"]
-kelp_data = kelp_data["timestamp", ]
+kelp_data = data.loc[data["product"] == "KELP", ["timestamp", "mid_price"]]
+
+plt.plot(kelp_data['timestamp'], kelp_data['mid_price'], 'b-', label='Mid Price', linewidth=2)
+
+# Customize
+plt.title('KELP vs RESIN: Order Book Prices Over Time', fontsize=14)
+plt.xlabel('Timestamp (ms)', fontsize=12)
+plt.ylabel('Price', fontsize=12)
+plt.legend()
+plt.grid(alpha=0.3)
+plt.show()
 
 
 print(kelp_data)
